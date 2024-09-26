accd3ab0274ba2cdf1a03898aeabe7483a7c3e76
priyanshjain117 <jainpriyanshu117@gmail.com>
2024-09-26 23:42:30 +0530

    basic layout ready

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import qr from "qr-image";
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
@@ -9,6 +16,12 @@ app.use(express.json());
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'./views'))
+
+app.get("/",(req,res)=>{
+  res.render("home");
+});
+
+
 
 const start = () => {
     try {
